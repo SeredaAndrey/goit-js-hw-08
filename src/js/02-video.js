9918205c58b580c1be7db30f6803c9dc7f1c7bee
Andrii Sereda <a.a.sereda@ukr.net>
2022-10-06 16:49:59 +0300

    woeweocvw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
 
+// const player = new Player('handstick', {
+//   width: 640,
+// });
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Vimeo.Player(iframe);
+
+// Select with jQuery
+// If multiple elements are selected, it will use the first element.
+const jqueryPlayer = new Vimeo.Player($('iframe'));
+
+// Select with the `<iframe>`’s id
+// Assumes that there is an <iframe id="player1"> on the page.
+const idPlayer = new Vimeo.Player('player1');
